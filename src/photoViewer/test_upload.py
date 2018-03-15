eebceb806db2e5c39ee734ace2d80adc056a7485
Ceyron Hawthorne <loremIpsum1771@users.noreply.github.com>
2018-03-15 17:07:44 -0400

    Update test_upload.py

--- src/photoViewer/test_upload.py
+++ src/photoViewer/test_upload.py
@@ -8,8 +8,9 @@ class FileUploadTestClass(TestCase):
     def test_uploaded_file(self):
         c = Client()
         originalFilePath = '../static_cdn/Keep-Calm-and-Carry-On.jpg'
-        image_data = open(originalFilePath, "rb")
+        #image_data = open(originalFilePath, "rb")
         with open(originalFilePath, "rb") as fp:
             response = c.post('/', {'image': fp})
-            self.assertEqual(image_data,response)
+            fp.seek(0)
+            self.assertEqual( fp.read(),response.content)
 
